040888e62d449387dd68aece1db9cca7d6f45c8f
Hertz-H <75604688+Hertz-H@users.noreply.github.com>
2022-03-17 22:26:07 +0300

    Add files via upload

--- query_builder/ch_pdo.php
+++ query_builder/ch_pdo.php
@@ -2,20 +2,22 @@
 class DatabaseClass{
     private $sql_stmt="";
     private $join="";
+    private $where="";
+    private $groupby="";
+    private $orderby="";
+    private $limit="";
+    private $counter=1;
+
+
+
+
     private $pdo;
     function __construct($dsn,$username,$password)
     {
         $this->pdo=new PDO($dsn,$username,$password);
         
     }
-    public function query_build(){
-        if($this->sql_stmt!=""){
-           $stmt=$this->pdo->prepare($this->sql_stmt.' '.$this->join);
-           $stmt->execute(); 
-           return $stmt->fetchAll(PDO::FETCH_OBJ);
-       }
-      
-    }
+  
 
    public function query($table,$sl_list=null,$where=null){
         if($sl_list!=null){
@@ -50,19 +52,73 @@ class DatabaseClass{
         
     }
   
-    public function groupby($table_Stmt,$type="inner",$condition=null){
-        $join_types=array("left","outer","right","inner");
-        if(in_array(strtolower($type),$join_types)){
-             $this->join.='  '.$type.' join '.$table_Stmt.' '.$condition;
-            }
+    public function orderby($sort=null){
+       if($sort!=null){
+           echo "not null";
+           foreach($sort as $key =>$value ){
+                if(is_int($key)&&(count($sort)==1)){
+                    echo "indexed";
+                        $this->orderby.=' '.'order by '.''.$value.' ' .'asc'.' , ' ;
+                        echo ' '.'order by '.''.$value.' ' .'desc'.' , ';
+                }
+                else{
+                    $this->orderby.=' '.'order by '.''.$key.' ' .$value.' , ' ;
+                    echo ' '.'order by '.''.$key.' ' .$value.' , ';
+                }
+           }
+           $this->orderby = substr( $this->orderby, 0, -2);  
+     
+       }
            return $this;
         
     }
-   
+    public function groupby($group_cols=null){
+        if($group_cols!=null){
+            $groupString=implode(',',$group_cols);
+            $this->groupby.=' '.'group by'.' '. $groupString;
+            echo ' '.'group by'.' '. $groupString;
+        }
 
+        return $this;
+    }
+    public function limit($limit_params=null){
+        if($limit_params!=null){
+            $limit_string=implode(',',$limit_params);
+            $this->limit.=' '.'limit'.' '. $limit_string;
+            echo ' '.' limit '.' '. $limit_string;
+        }
 
+        return $this;
+    }
+    
+    public function where($where_con_1,$link=null,$where_con_2=null){
+        $def_link='and';
+        $link_arr=array('and','or');
+        if(in_array(strtolower($link),$link_arr)||$link==null){
+            echo 'link is null';
+            if($this->counter>1){
+                $this->where.='  '. $def_link.' '.$where_con_1.' '.$link.'  '.$where_con_2;
+                echo 'counter>1';
+            }
+            else{
+                echo 'counter<1';
+                $this->where.='  '.'where'.' '.$where_con_1.' '.$link.' '.$where_con_2;
+                echo '  '.'where'.' '.$where_con_1.' '.$link.' '.$where_con_2;
+            }
+        $this->counter++;
+      }
+      return $this;
+    }
+  
 
-
+    public function query_build(){
+        if($this->sql_stmt!=""){
+           $stmt=$this->pdo->prepare($this->sql_stmt.' '.$this->join.' '.$this->where.' '.$this->groupby.' '.$this->orderby.' '.$this->limit);
+           $stmt->execute(); 
+           return $stmt->fetchAll(PDO::FETCH_OBJ);
+       }
+      
+    }
 
 
 
@@ -101,7 +157,20 @@ $where_condition = array(
     'id'     =>    7  
 );  
 
-$result=$db_obj->query("category")->join("products "," inner "," on category.id = 3")->query_build();//.join("products")
+// $result=$db_obj->query("category")->join("products "," inner "," on category.id = 3")->query_build();//.join("products")
+// $result=$db_obj->query("products")->orderby(array('id'=>'desc'))->query_build();
+// $result=$db_obj->query("products")->orderby(array('name'))->query_build();
+// $result=$db_obj->query("products")->groupby(array('active'))->orderby(array('name'))->query_build();
+// $result=$db_obj->query("products",array("count(id) as count"))->orderby(array('name'))->query_build();
+// $result=$db_obj->query("products")->limit(array('2','1'))->query_build();
+$result=$db_obj->query("products")->where('id = 64','or','name = "nokia"')->query_build();
+
+
+
+
+
+
+
 
    $insert_data = array(  
            'id'     =>    null,  
@@ -112,7 +181,9 @@ $result=$db_obj->query("category")->join("products "," inner "," on category.id 
 foreach($result as $row)
  {
   
-     echo "id  ".$row->id." name  ".$row->name." active  ".$row->active."<br>";
+     echo "<br>"."id  ".$row->id." name  ".$row->name." active  ".$row->active."<br>";
+    //  echo $row->count."<br>";
+
 
 }
 
